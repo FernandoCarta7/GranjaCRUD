a585697a541f4e50fe1d17242c37b64c592a63a6
Fernando Carta <cartafernando@outlook.com>
2025-09-01 17:36:35 -0500

    Inicializacion proyecto front y back

---
+++ LaGranjaSA/src/main/java/com/example/LaGranjaSA/LaGranjaSaApplication.java
@@ -0,0 +1,13 @@
+package com.example.LaGranjaSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaGranjaSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaGranjaSaApplication.class, args);
+	}
+
+}
---
+++ LaGranjaSA/src/test/java/com/example/LaGranjaSA/LaGranjaSaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LaGranjaSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaGranjaSaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
